7939e5a77353a8d1a17ad4358eb09f134d002545
Rowland1318 <christina.a.rowland@gmail.com>
2020-01-27 08:41:48 -0700

    spaces

--- src/com/skilldistillery/homework/MakeChange.java
+++ src/com/skilldistillery/homework/MakeChange.java
@@ -14,6 +14,7 @@ public class MakeChange {
 		welcomeQuestions();
 		amountCalculations(price, money);
 	}
+
 	public static void welcomeQuestions() {
 		System.out.println("What is the price of the item: ");
 		price = kb.nextDouble();
@@ -31,27 +32,28 @@ public class MakeChange {
 			System.out.println("You provided the exact amount");
 		} else if (money2 > price2) {
 			System.out.printf("Change provided %.2f %n", change);
-			if (change >= 20) {
-				int twentyAmount = (int) (change / 20);
-				change = (change % 20);
+			change = change * 100;
+			if (change >= 2000) {
+				int twentyAmount = (int) (change / 2000);
+				change = (change % 2000);
 				if (twentyAmount == 1) {
 					System.out.println(twentyAmount + " twenty dollar bill");
 				} else if (twentyAmount > 1) {
 					System.out.println(twentyAmount + " twenty dollar bills");
 				}
 			}
-			if (change >= 10) {
-				int tenAmount = (int) (change / 10);
-				change = (change % 10);
+			if (change >= 1000) {
+				int tenAmount = (int) (change / 1000);
+				change = (change % 1000);
 				if (tenAmount == 1) {
 					System.out.println(tenAmount + " ten dollar bill");
 				} else if (tenAmount > 1) {
 					System.out.println(tenAmount + " ten dollar bills");
 				}
 			}
-			if (change >= 5) {
-				int fiveAmount = (int) (change / 5);
-				change = (int) (change % 5);
+			if (change >= 500) {
+				int fiveAmount = (int) (change / 500);
+				change = (int) (change % 500);
 				if (fiveAmount == 1) {
 					System.out.println(fiveAmount + " five dollar bill");
 				} else if (fiveAmount > 1) {
@@ -59,18 +61,18 @@ public class MakeChange {
 				}
 
 			}
-			if (change >= 1) {
-				int oneAmount = (int) (change / 1);
-				change = (change % 1);
+			if (change >= 100) {
+				int oneAmount = (int) (change / 100);
+				change = (change % 100);
 				if (oneAmount == 1) {
 					System.out.println(oneAmount + " one dollar bill");
 				} else if (oneAmount > 1) {
 					System.out.println(oneAmount + " one dollar bills");
 				}
 			}
-			if (change >= .25) {
-				int quarterAmount = (int) (change / .25);
-				change = (change % .25);
+			if (change >= 25) {
+				int quarterAmount = (int) (change / 25);
+				change = (change % 25);
 				if (quarterAmount == 1) {
 					System.out.println(quarterAmount + " quarter");
 				} else if (quarterAmount > 1) {
@@ -78,25 +80,25 @@ public class MakeChange {
 				}
 			}
 			if (change >= .10) {
-				int dimeAmount = (int) (change / .10);
-				change = (change % .10);
+				int dimeAmount = (int) (change / 10);
+				change = (change % 10);
 				if (dimeAmount == 1) {
 					System.out.println(dimeAmount + " dime");
 				} else if (dimeAmount > 1) {
 					System.out.println(dimeAmount + " dimes");
 				}
 			}
-			if (change >= .05) {
-				int nickelAmount = (int) (change / .05);
-				change = (change % .05);
+			if (change >= 5) {
+				int nickelAmount = (int) (change / 5);
+				change = (change % 5);
 				if (nickelAmount == 1) {
 					System.out.println(nickelAmount + " nickel");
 				} else if (nickelAmount > 1) {
 					System.out.println(nickelAmount + " nickels");
 				}
 			}
-			if (change >= .001) {
-				int pennyAmount = (int) (change / .009);
+			if (change >= 1) {
+				int pennyAmount = (int) (change / 1);
 				if (pennyAmount == 1) {
 					System.out.println(pennyAmount + " penny");
 				} else if (pennyAmount > 1) {
